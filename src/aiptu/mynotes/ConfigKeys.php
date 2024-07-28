342c8369c1ef567c008fafaa8da1217cbe060756
AIPTU <anangp808@gmail.com>
2024-07-28 20:19:57 +0700

    fix: license header

--- src/aiptu/mynotes/ConfigKeys.php
+++ src/aiptu/mynotes/ConfigKeys.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * Copyright (c) 2024 HazardTeam
+ * Copyright (c) 2024 AIPTU
  *
  * For the full copyright and license information, please view
  * the LICENSE.md file that was distributed with this source code.
  *
- * @see https://github.com/HazardTeam/Lottery
+ * @see https://github.com/AIPTU/MyNotes
  */
 
 declare(strict_types=1);
